f02c8f022902c918690fb868de80aaa0315f6beb
Paul Semel <semelpaul@gmail.com>
2018-04-23 10:58:19 +0200

    added some assert + vector_append function
    Signed-off-by: Paul Semel <semelpaul@gmail.com>

--- vector.c
+++ vector.c
@@ -6,12 +6,14 @@ int main()
 	void *vec = vector_init(int);
 
 	for (int i = 0; i < 15; i++)
-		vector_set(vec, i, i);
+		vector_append(vec, i);
 
 	for (int i = 0; i < 15; i++) {
 		int tmp;
 		printf("pos: %d = %d\n", i, vector_get(vec, i, tmp));
 	}
+
+	vector_destroy(vec);
 
 	return 0;
 }
--- vector.h
+++ vector.h
@@ -8,12 +8,12 @@
 		struct __vector_ { 	     				\
 			type *vect;  		     			\
 			size_t length;		     			\
-			size_t max;					\
+			long max;					\
 		};				     			\
 		struct __vector_ *t = malloc(sizeof(*t)); 		\
 		t->vect = malloc(sizeof(type) * VECT_START_LENGTH);	\
 		t->length = VECT_START_LENGTH;				\
-		t->max = 0;						\
+		t->max = -1;						\
 		t;							\
 })
 
@@ -21,6 +21,8 @@
  * Unfortunately, I didn't find a way to do it without the container..
  */
 #define vector_get(vec, pos, container) ({			\
+		assert(vec &&					\
+		       "vector might no be NULL pointer");	\
 		struct __vector_{ 	     			\
 			typeof(container) *vect;  		\
 			size_t length;		     		\
@@ -31,6 +33,8 @@
 })
 
 #define vector_set(vec, pos, val) do {					    \
+	assert(vec &&							    \
+	       "vector might no be NULL pointer");		    	    \
 	struct __vector_ { 	     					    \
 		typeof(val) *vect;  					    \
 		size_t length;		     				    \
@@ -42,8 +46,39 @@
 		__tmp->vect = realloc(__tmp->vect, 			    \
 				      sizeof(*__tmp->vect) * __tmp->length);\
 	}								    \
-	if (pos >= __tmp->max)						    \
+	if (pos > __tmp->max)						    \
 		__tmp->max = pos;					    \
 									    \
 	__tmp->vect[pos] = val; 					    \
 } while(0)
+
+#define vector_destroy(vec) do {				\
+	assert(vec &&						\
+	       "vector might no be NULL pointer");	  	\
+	struct __vector_ { 	     				\
+		void *vect;  					\
+		size_t length;		     			\
+		size_t max;					\
+	};				     			\
+	struct __vector_ *__tmp = (void *)vec; 			\
+	free(__tmp->vect);					\
+	free(__tmp);						\
+} while(0)
+
+#define vector_append(vec, val) do {					    \
+	assert(vec &&							    \
+	       "vector might no be NULL pointer");	  		    \
+	struct __vector__ { 	     					    \
+		typeof(val) *vect;  					    \
+		size_t length;		     				    \
+		size_t max;						    \
+	};				     				    \
+	struct __vector__ *__tmp = (void *)vec; 			    \
+	if (__tmp->max >= __tmp->length - 1) { 				    \
+		__tmp->length *= 2; 					    \
+		__tmp->vect = realloc(__tmp->vect, 			    \
+				      sizeof(*__tmp->vect) * __tmp->length);\
+	}								    \
+	__tmp->max++;							    \
+	__tmp->vect[__tmp->max] = val;					    \
+} while (0)
